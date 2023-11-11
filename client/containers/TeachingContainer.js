7a84296affab0d8501ed81f03632ef6b0c5dc02e
Natalie Pentschev <67292089+astr0-nat@users.noreply.github.com>
2023-11-10 16:37:12 -0800

    created new component to display text lesson  title and description. utilized MUI Grid to have comments section fit next to it.

--- client/containers/TeachingContainer.js
+++ client/containers/TeachingContainer.js
@@ -1,30 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import LessonPost from '../components/LessonPost';
+import TextTeachingDisplay from '../components/TextTeachingDisplay';
 
 function TeachingContainer() {
-	const[lessons, setLessons] = useState([]);
+	const { id } = useParams();
+	const[lesson, setLesson] = useState([]);
+	// lesson id
 	useEffect(() => {
 
-		// Fetch threads
-		fetch("http://localhost:3000/api/teaching-texts/")
+		// Fetch lesson
+		console.log(id);
+		fetch(`http://localhost:3000/api/teaching-texts/${id}`)
 			.then((resp) =>
 			resp.json())
 			.then((value) => {
-				console.log(value);
-				setLessons(value);
+				setLesson(value);
 			});
 
 			// fetch("http://localhost:3000/api/threads/?lessonId=${lessonId}")
 			// .then((resp) => resp.json())
 			// .then(setThreads)
 
-	}, []);
+	}, [id]);
 	// const threadCards = threads.map((thread) => <div><p>{thread.content}</p></div>)
-	const lessonLinks = lessons.map((lesson)=> <div><p>{lesson.title}</p></div>)
+	// const lessonLinks = lessons.map((lesson)=> <div><p>{lesson.title}</p></div>)
 	return (
 		<div>
-			{lessonLinks}
-			{/* teaching content component here */}
-			{/* discussion thread component here */}
+			<TextTeachingDisplay title={lesson.title} description={lesson.description}></TextTeachingDisplay>
+{/* 			
+			<h1> {lesson.title} </h1>
+			<p>{lesson.description}</p> */}
+			{/* <LessonPost title={lesson.title} desc={lesson.description} id={lesson._id}></LessonPost> */}
 		</div>
 	);
 
